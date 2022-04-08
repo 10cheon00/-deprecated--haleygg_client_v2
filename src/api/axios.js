8975c44a5b99d81a93ad6a67b483d7f762c18a9e
10cheon00 <10cheon00@gmail.com>
2022-04-08 18:24:48 +0900

    Fix: Modified new server url.

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,11 +3,10 @@ import axios from "axios";
 import vuexStore from "@/store/store.js";
 import router from "@/router/index.js";
 
-// const developServerUrl = "http://127.0.0.1:8000"
-const deployServerUrl = "https://api.haleygg.kr/"
+const serverUrl = "https://haleygg.10cheon00.xyz/"
 
 const axiosInstance = axios.create({
-  baseURL: deployServerUrl,
+  baseURL: serverUrl,
   timeout: 5000,
 })
 
