9a4bf7acde425f126cd7edfa1e415926c969f65b
10cheon00 <10cheon00@gmail.com>
2022-05-11 11:29:58 +0900

    feat: Create rank board on HomeView
    Created PlayerRankBoard component. It will be use proleague statistics.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,30 +8,65 @@
     </PageHeader>
 
     <div class="container p-3">
-      <div id="info">
-        <p class="p-1">
-          카페에 가입하신 닉네임을 검색하시면 자신의 전적을 확인할 수 있습니다.
-        </p>
-        <p class="p-1">
-          현재 개발하고 있는 중입니다... 조금만 더 기다려주세요.
-        </p>
+      <div id="information">
+        <p>할리 클랜의 공식전 전적을 조회할 수 있는 사이트입니다.</p>
+        <p>닉네임을 검색하시면 관련된 전적과 통계를 볼 수 있습니다.</p>
+      </div>
+      <div class="grid grid-nogutter">
+        <PlayerRankBoard
+          v-if="totalMatchCountRank"
+          class="col-12 md:col-6 pr-0 md:pr-2 pb-2 md:pb-0"
+          :rankSheet="totalMatchCountRank"
+          title="통합 다전 순위"
+          unit="경기"
+        />
+        <PlayerRankBoard
+          v-if="totalWinCountRank"
+          class="col-12 md:col-6 pl-0 md:pl-2"
+          :rankSheet="totalWinCountRank"
+          title="통합 다승 순위"
+          unit="승"
+        />
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import { defineComponent } from "vue";
+import { defineComponent, ref, onMounted } from "vue";
 
 import PageHeader from "@/components/PageHeader.vue";
 import PlayerSearchBar from "@/components/PlayerSearchBar.vue";
+import PlayerRankBoard from "@/components/PlayerRankBoard.vue";
+import ServerApi from "@/api/server/module.js";
 
 export default defineComponent({
   components: {
     PageHeader,
+    PlayerRankBoard,
     PlayerSearchBar,
   },
-  setup() {},
+  setup() {
+    const totalMatchCountRank = ref(null);
+    const totalWinCountRank = ref(null);
+
+    const fetchRank = async () => {
+      let response = await ServerApi.fetchTotalMatchCountRank();
+      totalMatchCountRank.value = response.data.results;
+
+      response = await ServerApi.fetchTotalWinCountRank();
+      totalWinCountRank.value = response.data.results;
+    };
+
+    onMounted(() => {
+      fetchRank();
+    });
+
+    return {
+      totalMatchCountRank,
+      totalWinCountRank,
+    };
+  },
 });
 </script>
 
@@ -49,9 +84,11 @@ export default defineComponent({
   color: white;
 }
 
-#info {
-  text-align: center;
+#information {
+  border-bottom: 1px solid #dee2e6;
+  margin-bottom: 1rem;
   padding: 4rem 0;
+  text-align: center;
 }
 
 #search-bar {
