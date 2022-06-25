8b49274579ba477990d43f781895b576d3bb7366
10cheon00 <10cheon00@gmail.com>
2022-06-25 18:39:44 +0900

    refactor: Combine methods in validator.js using ES6 class keyword

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -1,4 +1,104 @@
-import { computed } from "vue";
+import { computed, readonly, ref } from "vue";
+
+
+class Validator {
+  #isTriggered;
+  #state;
+  #rules;
+  #errorObj;
+
+  constructor(state, rules) {
+    this.#state = state;
+    this.#rules = rules;
+    this.#errorObj = readonly(this.__initializeErrorObj__(this.#state, this.#rules));
+
+    this.#isTriggered = ref(false);
+  }
+
+  __initializeErrorObj__ = (state, rules) => {
+    const result = Object.keys(state).reduce((acc, key) => {
+      // check value is array.
+      if (Array.isArray(state[key])) {
+        acc[key] = {
+          $this: computed(() => {
+            return this.__runValidaionFunctions__(state[key], rules[key].$this);
+          }),
+          $child: state[key].reduce((_acc, _cur) => {
+            _acc.push(this.__initializeErrorObj__(_cur, rules[key].$child));
+            return _acc;
+          }, new Array())
+        };
+      } else {
+        // otherwise create computed property.
+        if (rules[key] == undefined) {
+          acc[key] = {
+            hasError: false,
+            message: ""
+          };
+        }
+        else {
+          acc[key] = computed(() => {
+            return this.__runValidaionFunctions__(state[key], rules[key]);
+          });
+        }
+      }
+      return acc;
+    }, new Object());
+    return result;
+  }
+
+  __runValidaionFunctions__ = (state, validationFunctions) => {
+    if (this.#isTriggered.value) {
+      for (const key in validationFunctions) {
+        const runValidation = validationFunctions[key];
+        const errorMessage = runValidation(state);
+        if (errorMessage) {
+          return {
+            hasError: true,
+            message: errorMessage
+          };
+        }
+      }
+    }
+    return {
+      hasError: false,
+      message: ""
+    };
+  }
+
+  validate = () => {
+    this.#isTriggered.value = true;
+  }
+
+  get errorObj() {
+    return this.#errorObj;
+  }
+
+  hasError = () => {
+    return getError(this.#errorObj);
+  }
+
+  __getError__ = (errorObj) => {
+    const val = Object.keys(errorObj).reduce((result, key) => {
+      // check erorrObj is array
+      if (Object.prototype.hasOwnProperty.call(errorObj[key], "$child")) {
+        result = errorObj[key].$this.hasError || result;
+        // call getError method recursively on each child errorObj.
+        result = errorObj[key].$child.reduce(
+          (_result, erorrObjRef) => {
+            _result = getError(erorrObjRef) || _result;
+            return _result;
+          }, false
+        ) || result;
+      } else {
+        result = errorObj[key].hasError || result;
+      }
+      return result;
+    }, false);
+    return val;
+  }
+}
+
 
 const Required = (state) => {
   if (state) {
@@ -12,111 +112,8 @@ const Required = (state) => {
   return "이 항목은 필수입니다."
 }
 
-const validate = (form) => {
-  form.errorObj = updateErrorObj(form.state, form.rules);
-}
-
-const updateErrorObj = (state, rules) => {
-  return Object.keys(state).reduce((acc, key) => {
-    // check value is array
-    if (Array.isArray(state[key])) {
-      acc[key] = {
-        $this: computed(() => {
-          return runValidators(state[key], rules[key].$this);
-        }),
-        $child: state[key].reduce((_acc, _cur) => {
-          _acc.push(updateErrorObj(_cur, rules[key].$child));
-          return _acc;
-        }, new Array())
-      };
-    } else {
-      // otherwise create computed property.
-      if (rules[key] == undefined) {
-        acc[key] = {
-          isErrorExists: false,
-          message: ""
-        }
-      }
-      else {
-        acc[key] = computed(() => {
-          return runValidators(state[key], rules[key]);
-        });
-      }
-    }
-    return acc;
-  }, new Object());
-};
-
-const runValidators = (state, validationFunctions) => {
-  for (const key in validationFunctions) {
-    const validate = validationFunctions[key];
-    const errorMessage = validate(state);
-    if (errorMessage) {
-      return {
-        isErrorExists: true,
-        message: errorMessage
-      }
-    }
-  }
-  return {
-    isErrorExists: false,
-    message: ""
-  }
-}
-
-const initializeErrorObj = (state) => {
-  const result = Object.keys(state).reduce((acc, key) => {
-    // check value is array.
-    if (Array.isArray(state[key])) {
-      acc[key] = {
-        $this: {
-          isErrorExists: false,
-          message: ""
-        },
-        $child: state[key].reduce((_acc, _cur) => {
-          _acc.push(initializeErrorObj(_cur));
-          return _acc;
-        }, new Array())
-      };
-    } else {
-      // otherwise, create error object.
-      acc[key] = {
-        isErrorExists: false,
-        message: "",
-      };
-    }
-    return acc;
-  }, new Object());
-  return result;
-}
-
-const isErrorExists = (form) => {
-  return getError(form.errorObj)
-}
-
-const getError = (errorObj) => {
-  const val = Object.keys(errorObj).reduce((result, key) => {
-    // check erorrObj is array
-    if (Object.prototype.hasOwnProperty.call(errorObj[key], "$child")) {
-      result = errorObj[key].$this.isErrorExists || result;
-      // call getError method recursively on each child errorObj.
-      result = errorObj[key].$child.reduce(
-        (_result, erorrObjRef) => {
-          _result = getError(erorrObjRef) || _result;
-          return _result;
-        }, false
-      ) || result;
-    } else {
-      result = errorObj[key].isErrorExists || result;
-    }
-    return result;
-  }, false);
-  return val;
-};
 
 export {
-  initializeErrorObj,
-  isErrorExists,
-  validate,
   Required,
+  Validator,
 } 
